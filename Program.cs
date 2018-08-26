c0fef720624701cf721613db1cd1c9da99c0802f
CodeAndCode <39539606+dulalkabin99@users.noreply.github.com>
2018-08-26 13:17:08 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSharp1
+namespace HelloWorld.Net
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            String hw = "Hello";
+            String name = "Kabin";
+
+            uint noNegative = 65U;//this can only store Positive values, Need to add U/u
+            sbyte sb = 125;//Signed 8 bit integer -128 to 127
+
+            Console.WriteLine(hw + " " + name);
+            Console.WriteLine("The unsigned uint Assigned = " + noNegative);
+            Console.WriteLine("the Signed sbyte Number = " + sb);
+            Console.ReadLine();
         }
     }
 }
